83a6fa17cff95c4dea270590934462699ebebeb1
Oliver Slater <oliver.slater+hashicorp@nordcloud.com>
2024-06-22 13:21:31 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -18,7 +18,6 @@ resource "aws_vpc" "hashicat" {
   tags = {
     name = "${var.prefix}-vpc-${var.region}"
     environment = "Production"
-    department = "devops"
   }
 }
 
@@ -129,6 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
+    Department = "devops"
+    Billable = "true"
   }
 }
 
